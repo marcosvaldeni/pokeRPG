58e2cc366945d8a6edeb77d7ee910095b7019f3e
Marcos Lucas <marcosvaldeni@gmail.com>
2021-06-05 17:43:21 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,66 @@
+import { 
+  FaInstagram,
+  FaTwitterSquare,
+  FaDiscord,
+  FaTwitch,
+  FaYoutubeSquare
+} from 'react-icons/fa';
+
 const Footer: React.FC = () => {
   return (
-    <section className="footer">
-      <h1>footer</h1>
+    <footer className="footer">
+      <div className="container">
+     
+        <div>
+          <div className="footer-desc">
+            <h1>PokeRPG</h1>
+
+            <p>
+              The best design for your web studio website. Vitae nulla nunc euismod 
+              vel pretium tellus accumsan semper. Vitae nulla nunc euismod vel 
+              pretium tellus accumsan semper. 
+            </p>
+          </div>
+
+          <div  className="footer-links">
+
+            <div>
+              <h2>Pages</h2>
+
+              <ul>
+                <li>Home</li>
+                <li>Download</li>
+                <li>Support us</li>
+                <li>Community</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+
+            <div>
+              <h2>Follow us</h2>
+              <ul>
+                <li><FaInstagram /></li>
+                <li><FaTwitterSquare /></li>
+                <li><FaDiscord /></li>
+                <li><FaTwitch /></li>
+                <li><FaYoutubeSquare /></li>
+              </ul>
+
+              <h2>Our Illustrator</h2>
+              <p>Boone Carlyle</p>
+            </div>
+
+          </div>
+        </div>
 
 
-    </section>
+        <div className="footer-info">
+          <p>2021 © All Rights Reserved.</p>
+          <p>UX by <strong>Marcos Valdeni Lucas</strong></p>
+        </div>
+
+      </div>
+    </footer>
   );
 }
 
